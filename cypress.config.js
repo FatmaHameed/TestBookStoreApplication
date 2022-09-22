856621f40628b4a71447770c011fe6107a892509
Fatma Hameed <fatma78hameed@gmail.com>
2022-09-22 14:56:33 -0700

    Modified cypress.config.js file

--- cypress.config.js
+++ cypress.config.js
@@ -25,7 +25,7 @@ module.exports = defineConfig({
     },
 
     baseUrl: 'https://demoqa.com',
-    specPattern: '**/*.feature',
+    specPattern: 'cypress/e2e/smokeTestProfile/*.feature',
     chromeWebSecurity: false,
     viewportWidth: 1200,
     viewportHeight: 800,
